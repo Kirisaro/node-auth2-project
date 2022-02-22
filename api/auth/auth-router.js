8ceb0e24f2db41fca60457b9c7179c1f8a4bd074
Carlos Solano <solanoandy@gmail.com>
2022-02-22 16:08:27 -0600

    internet and pc problems not letting me run tests

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { BCRYPT_ROUNDS } = require("../secrets"); // use this secret!
+const UM = require('../users/users-model')
+const bcrypt = require('bcrypt');
+const tokenCreator = require('./tokenCreator')
 
-router.post("/register", validateRoleName, (req, res, next) => {
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
     response:
     status 201
     {
@@ -13,30 +14,47 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "username": "anna",
       "role_name": "angel"
     }
-   */
+  **/
+router.post("/register", validateRoleName, (req, res, next) => {
+  let addUsers = req.body
+  const hash = bcrypt.hashSync(addUsers.password, BCRYPT_ROUNDS)
+  addUsers.password = hash
+
+  UM.add(addUsers)
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(next)
 });
 
-
-router.post("/login", checkUsernameExists, (req, res, next) => {
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
     response:
     status 200
     {
       "message": "sue is back!",
       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
     }
-
     The token must expire in one day, and must provide the following information
     in its payload:
-
     {
       "subject"  : 1       // the user_id of the authenticated user
       "username" : "bob"   // the username of the authenticated user
       "role_name": "admin" // the role of the authenticated user
     }
-   */
+  **/
+router.post("/login", checkUsernameExists, (req, res, next) => {
+  let { username, password } = req.body
+  UM.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenCreator(user)
+        res.status(200).json({ message: `${user.username} is back`, token })
+      } else {
+        next({ status: 401, message: 'Invalid Credentials' })
+      }
+    })
+    .catch(next)
 });
 
 module.exports = router;
